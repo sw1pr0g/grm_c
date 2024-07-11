f633d3b043578d57c4637a68f45fa231a71ca58a
SW1pr0g <poryadin_alex@mail.ru>
2024-07-11 15:33:39 +0300

    Add vuetify

--- client/.eslintrc.js
+++ client/.eslintrc.js
@@ -1,17 +1,20 @@
+/**
+ * .eslint.js
+ *
+ * ESLint configuration file.
+ */
+
 module.exports = {
   root: true,
   env: {
-    node: true
+    node: true,
   },
-  'extends': [
+  extends: [
     'plugin:vue/vue3-essential',
-    'eslint:recommended'
+    'eslint:recommended',
+    '@vue/eslint-config-typescript',
   ],
-  parserOptions: {
-    parser: '@babel/eslint-parser'
-  },
   rules: {
-    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
-  }
+    'vue/multi-word-component-names': 'off',
+  },
 }
