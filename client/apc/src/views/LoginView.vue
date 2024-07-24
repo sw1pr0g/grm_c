94a478fab8441b61650b98c604ed5666113c60d3
Alex Chufarin <chufarin2004@mail.ru>
2024-07-25 00:00:06 +0300

    fix login page

---
+++ client/apc/src/views/LoginView.vue
@@ -0,0 +1,27 @@
+<template>
+  <v-app>
+    <LoginForm />
+  </v-app>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+import LoginForm from '../components/LoginForm.vue'
+
+export default defineComponent({
+  name: 'LoginView',
+  components: {
+    LoginForm
+  }
+})
+</script>
+
+<style scoped>
+.v-container {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 200vh;
+  width: 300vw;
+}
+</style>
