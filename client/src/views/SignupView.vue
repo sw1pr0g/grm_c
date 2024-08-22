f7b338210952f8f2110f63f54a1b55c94ce7c6ad
Nick <nikcccck@gmail.com>
2024-08-22 23:31:46 +0300

    fixed sign up

---
+++ client/src/views/SignupView.vue
@@ -0,0 +1,21 @@
+<template>
+    <v-app>
+      <v-main>
+        <v-container class="container">
+          <signup-form />
+        </v-container>
+      </v-main>
+    </v-app>
+  </template>
+  
+  <script lang="ts">
+  import { defineComponent } from 'vue'
+  import SignupForm from '@/components/SignupForm.vue'
+  
+  export default defineComponent({
+    name: 'SignupView',
+    components: {
+      SignupForm
+    }
+  })
+  </script>
